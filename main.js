164b9c08571a0a6faee146f0b35d19063d199a4c
saverioannpy <saverio.annunziata@outlook.com>
2023-11-29 03:46:38 +0100

    feat: aggiunta commenti

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ const myApp = createApp({
             this.todoList.push(obj);
             taskInput.value = "";
         },
-
         unlineTask(){
             const x = event.target.innerText;
             for(let i = 0 ; i <= this.todoList.length-1 ; i++){ 
@@ -31,11 +30,10 @@ const myApp = createApp({
             }
         },
         deleteTask(){
-            let x = event.target;
-            x = x.parentElement.innerText;
-            x = x.split('\n')[0];
-            this.todoList = this.todoList.filter( (el) => el.taskName != x );
-            console.log(this.todoList);
+            let x = event.target; //salvo in una variabile il target dell'evento click
+            x = x.parentElement.innerText; //ho gestito il codice HTML in modo tale da poter risalire al div padre dell'icona X tramite l'evento
+            x = x.split('\n')[0]; //l'Innertext del padre è composto da una stringa del tipo:"testoTask\nX", con split elimino ciò che si trova dopo il \n ed ottengo solo il testo digitato dall'utente
+            this.todoList = this.todoList.filter( (el) => el.taskName != x ); //riformulo il nostro array eliminando il testo presente nella variabile X (i doppioni vengono elimianti)
         }
     }
 });
